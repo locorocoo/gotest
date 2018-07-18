3e965ff6ca77bd5546b6780aa931bb3be8b5e558
locorocoo <chen.yi@berkeley.edu>
2018-07-18 15:19:13 -0700

    by yi

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X yooooooo golang http example"
+	title := "Jenkins X golang http example by yi"
 
 	from := ""
 	if r.URL != nil {
